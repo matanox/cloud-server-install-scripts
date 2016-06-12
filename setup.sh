4b457ca64c4bcf6f42fedb3e615ec8944d8524d2
matan <matan@superup.me>
2016-06-12 22:37:33 +0300

    authbind

--- setup.sh
+++ setup.sh
@@ -11,14 +11,24 @@ echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selec
 apt-get install oracle-java8-installer -y
 apt-get install oracle-java8-set-default
 
-# git
+# git ― you will kind of already have git installed if you cloned this repo to the cloud server...
 apt-get install git -y
 
 # sbt
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
-sudo apt-get update
-sudo apt-get install sbt -y
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
+apt-get update
+apt-get install sbt -y
+
+# open up standard web server ports
+apt-get install authbind -y
+touch /etc/authbind/byport/80
+chmod 755 /etc/authbind/byport/80
+touch /etc/authbind/byport/443
+chmod 755 /etc/authbind/byport/443
 
 # htop
 apt-get install htop -y
+
+ECHO now start sbt so that it downloads itself over the Internet... this may take a minute or two to complete
+# TODO: optionally take arguments for https git clone uri and clone & compile that repo in the end (as first compile updates all dependencies which takes several minutes)
