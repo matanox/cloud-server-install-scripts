91eaf413002484e7a1549ca9e0dcf208ed411855
matan <matan@superup.me>
2016-06-12 18:24:32 +0300

    initial commit

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#
+# cloud server setup script
+#
+
+# Java 8
+apt-get update
+apt-get install software-properties-common -y
+add-apt-repository ppa:webupd8team/java -y
+apt-get update
+echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
+apt-get install oracle-java8-installer -y
+apt-get install oracle-java8-set-default
+
+# git
+apt-get install git
+
+# sbt
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
+sudo apt-get update
+sudo apt-get install sbt
+
+# htop
+apt-get install htop
