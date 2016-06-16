5bf2630f6e4246b1ab21d5ce442d73f61c835145
matan <matan@superup.me>
2016-06-16 17:05:49 +0300

    audio decoders

--- setup.sh
+++ setup.sh
@@ -30,5 +30,12 @@ chmod 755 /etc/authbind/byport/443
 # htop
 apt-get install htop -y
 
+# audio conversion from mp4 to wav ― specific to Ubuntu 14.04
+sudo add-apt-repository ppa:mc3man/trusty-media -y
+sudo apt-get install ffmpeg -y
+
+# audio conversion from opus to wav (was it enough?)
+sudo apt-get install opus-tools -y
+
 ECHO now start sbt so that it downloads itself over the Internet... this may take a minute or two to complete
 # TODO: optionally take arguments for https git clone uri and clone & compile that repo in the end (as first compile updates all dependencies which takes several minutes)
